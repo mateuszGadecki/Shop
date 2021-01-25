a54e1420dc04ba293a68c39531875c1bea145378
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-01-25 17:41:39 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import Layout from "./hoc/Layout/Layout";
+
+class App extends Component {
+  render() {
+    return <Layout></Layout>;
+  }
+}
+
+export default App;
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Layout extends Component {
+  render() {
+    return <p>Init</p>;
+  }
+}
+
+export default Layout;
