95bdc5e4192fb0de53ae949b03f1763aff97cca5
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-02-07 18:22:30 +0100

    Changing the FontSize from Pixels to REM

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import HomePage from "./components/HomePage/HomePage";
 import AboutUs from "./components/AboutUs/AboutUs";
+import Contact from "./components/Contact/Contact";
 
 class App extends Component {
   render() {
@@ -12,6 +13,9 @@ class App extends Component {
         <Switch>
           <Route path="/aboutUs">
             <AboutUs />
+          </Route>
+          <Route path="/contact">
+            <Contact />
           </Route>
           <Route path="/" exact>
             <HomePage />
