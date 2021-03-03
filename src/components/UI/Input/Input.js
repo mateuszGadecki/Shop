8e9d3b22d1916a6fb2cb20818389b3753af4dec8
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-03-03 18:26:02 +0100

    Updating the reusable input by adding validation

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,20 +3,31 @@ import React from "react";
 import classes from "./Input.module.css";
 
 const input = (props) => {
-  const inputClasses = [];
+  let inputClass, highlightClass, barClass, labelClass;
+  inputClass = classes.inputBlue;
+  highlightClass = classes.highlightBlue;
+  barClass = classes.barBlue;
+  labelClass = classes.labelBlue;
+
+  if (props.invalid && props.shouldValidate) {
+    inputClass = classes.inputRed;
+    highlightClass = classes.highlightRed;
+    barClass = classes.barRed;
+    labelClass = classes.labelRed;
+  }
   return (
     <div className={classes.group}>
       <input
+        className={inputClass}
         style={{ backgroundColor: props.background }}
-        className={inputClasses.join(" ")}
         {...props.elementConfig}
         value={props.value}
         onChange={props.changed}
         required
       />
-      <span className={classes.highLight}></span>
-      <span className={classes.bar}></span>
-      <label>{props.label}</label>
+      <span className={highlightClass}></span>
+      <span className={barClass}></span>
+      <label className={labelClass}>{props.label}</label>
     </div>
   );
 };
