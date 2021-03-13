bb01c541199ea6569e395f8e2a7a0e1a776c3fe6
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-03-13 16:30:02 +0100

    Changing appearance of the homePage, changing the font in nav and footer

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,61 +1,17 @@
 import React, { Component } from "react";
 
 import classes from "./HomePage.module.css";
+import video from "../../assets/videos/background_film.mov";
 
 class HomePage extends Component {
-  state = {
-    hover: false,
-  };
-
-  hoverHandler = () => {
-    this.setState({ hover: true });
-  };
-
-  exitHoverHandler = () => {
-    this.setState({ hover: false });
-  };
-
   render() {
-    let hoverClassName = [];
-    if (this.state.hover) {
-      hoverClassName = [classes.p_elem, classes.hoverClass].join(" ");
-    } else {
-      hoverClassName = [classes.p_elem];
-    }
-
     return (
       <div className={classes.HomePage}>
-        <div className={classes.perspective_text}>
-          <div className={classes.perspective_line}>
-            <p className={hoverClassName}></p>
-            <p className={hoverClassName}>Aparat</p>
-          </div>
-          <div className={classes.perspective_line}>
-            <p className={hoverClassName}>Aparat</p>
-            <p className={hoverClassName}>to przycisk</p>
-          </div>
-          <div className={classes.perspective_line}>
-            <p className={hoverClassName}>to przycisk</p>
-            <p className={hoverClassName}>ZAPISZ</p>
-          </div>
-          <div className={classes.perspective_line}>
-            <p className={hoverClassName}>ZAPISZ</p>
-            <p className={hoverClassName}>dla oka umysłu</p>
-          </div>
-          <div className={classes.perspective_line}>
-            <p className={hoverClassName}>dla oka umysłu</p>
-            <p className={hoverClassName}></p>
-          </div>
-        </div>
-        <div className={classes.ButtonContainer}>
-          <button
-            onMouseEnter={this.hoverHandler}
-            onMouseLeave={this.exitHoverHandler}
-            className={classes.HomePageButton}
-          >
-            Poznaj naszą ofertę
-          </button>
-          <span className={classes.Line}></span>
+        <video className={classes.video} autoPlay loop muted>
+          <source src={video} type="video/mp4" />
+        </video>
+        <div className={classes.HomePageTitle}>
+          Najlepszy aparat to ten, <br /> który masz przy sobie.
         </div>
       </div>
     );
